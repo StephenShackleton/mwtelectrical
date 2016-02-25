6752e83db512ae9a96254a667c5539263fabbd12
Stephen Shackleton <stephen.shackleton@victoriaplum.com>
2016-02-25 21:22:31 +0000

    Splitting into partials

--- app/Http/Controllers/Store/HomeController.php
+++ app/Http/Controllers/Store/HomeController.php
@@ -1,14 +1,18 @@
 <?php namespace App\Http\Controllers\Store;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\View;
 
 /**
-*
-*/
+ * @package App\Http\Controllers\Store
+ */
 class HomeController extends Controller
 {
+	/**
+	 * @return View
+	 */
 	public function index()
 	{
-		return view('welcome');
+		return view('desktop.home.index');
 	}
 }
---
+++ resources/views/desktop/home/carousel.blade.php
@@ -0,0 +1,35 @@
+<header id="homepage-carousel" class="carousel slide">
+    <ol class="carousel-indicators">
+        <li data-target="#homepage-carousel" data-slide-to="0" class="active"></li>
+        <li data-target="#homepage-carousel" data-slide-to="1"></li>
+        <li data-target="#homepage-carousel" data-slide-to="2"></li>
+    </ol>
+
+    <div class="carousel-inner">
+        <div class="item active">
+            <div class="fill" style="background-image:url('http://mwtelectricalservices.co.uk/images/demo/slider/4.png');"></div>
+            <div class="carousel-caption">
+                <h2>Household!?!?</h2>
+            </div>
+        </div>
+        <div class="item">
+            <div class="fill" style="background-image:url('http://mwtelectricalservices.co.uk/images/demo/slider/1.png');"></div>
+            <div class="carousel-caption">
+                <h2>Commercial</h2>
+            </div>
+        </div>
+        <div class="item">
+            <div class="fill" style="background-image:url('https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/994228_10151784183315763_1890680839_n.jpg?oh=073ce6884c8e1bcebddbe37e36d921cf&oe=576C6188');"></div>
+            <div class="carousel-caption">
+                <h2>Caption 3</h2>
+            </div>
+        </div>
+    </div>
+
+    <a class="left carousel-control" href="#homepage-carousel" data-slide="prev">
+        <span class="icon-prev"></span>
+    </a>
+    <a class="right carousel-control" href="#homepage-carousel" data-slide="next">
+        <span class="icon-next"></span>
+    </a>
+</header>
